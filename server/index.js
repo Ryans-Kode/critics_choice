5b13745fe1033818aadcf706d008eb7096083231
Ryan <theryanrojas@gmail.com>
2023-12-28 15:01:10 -0800

    var typo

--- server/index.js
+++ server/index.js
@@ -14,6 +14,6 @@ app.get('/data.json', (req, res) => {
 });
 
 // Start the server
-app.listen(port, hostname, () => {
+app.listen(PORT, HOSTNAME, () => {
   console.log(`Server is running on http://${HOST}:${PORT}`);
 });
