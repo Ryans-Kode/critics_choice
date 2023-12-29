c172bdb7f6292d9c44ead666569c0457c6baf73e
Ryan <theryanrojas@gmail.com>
2023-12-29 10:09:11 -0800

    added missing files from react build

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const filePath = path.join(__dirname, 'movie_data', 'winnersOnNetflix.json');
 
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
