e51706a51e41659c6517445f119b26deb8ace35f
Ryan <theryanrojas@gmail.com>
2024-01-03 21:16:24 -0800

    origin fix

--- server/server.js
+++ server/server.js
@@ -19,8 +19,6 @@ app.get('/data.json', (req, res) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    res.setHeader('Content-Type', 'application/json');
-
     res.sendFile(filePath);
 });
 
